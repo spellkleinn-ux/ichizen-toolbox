fa71121ca2ccb617d674a98a85da4601907148e2
spellkleinn-ux <spellkleinn@gmail.com>
2026-04-24 05:43:26 +0200

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './pages/Home.jsx'
+import ToolDetail from './pages/ToolDetail.jsx'
+import Submit from './pages/Submit.jsx'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/"           element={<Home />} />
+        <Route path="/tool/:slug" element={<ToolDetail />} />
+        <Route path="/submit"     element={<Submit />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
